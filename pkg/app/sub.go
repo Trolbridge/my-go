87f56ba0b20aa0f498139afb1b9504facf7b5c85
Trolbridge <trolbridge.x@gmail.com>
2021-05-07 18:49:41 -0400

    Auto-commit: saved pkg/app/sub.go

--- pkg/app/sub.go
+++ pkg/app/sub.go
@@ -13,21 +13,6 @@ func PrintLine() {
 	println("print line test message")
 }
 
-//func Listen() []byte {
-//	// disable input buffering
-//	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-//	// do not display entered characters on the screen
-//	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
-
-//	// restore the echoing state when exiting
-//	defer exec.Command("stty", "-F", "/dev/tty", "echo").Run()
-
-//	var b []byte = make([]byte, 1)
-//	return b
-// Added this line
-
-//}
-
 // fd 0 is stdin
 func Listen() {
 	state, err := terminal.MakeRaw(0)
