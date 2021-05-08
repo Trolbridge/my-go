d98040fb16413d1825fea523a8f02963c903047f
Trolbridge <trolbridge.x@gmail.com>
2021-05-07 21:14:08 -0400

    Auto-commit: saved pkg/app/sub.go

--- pkg/app/sub.go
+++ pkg/app/sub.go
@@ -5,22 +5,20 @@ import (
 	"fmt"
 	"log"
 	"os"
-
-	"golang.org/x/crypto/ssh/terminal"
 )
 
 // fd 0 is stdin
 func Listen() rune {
-	// fd 0 is stdin
-	state, err := terminal.MakeRaw(0)
-	if err != nil {
-		log.Fatalln("Error: setting stdin to raw:", err)
-	}
-	defer func() {
-		if err := terminal.Restore(0, state); err != nil {
-			log.Println("Error: failed to restore terminal:", err)
-		}
-	}()
+	//	// fd 0 is stdin
+	//	state, err := terminal.MakeRaw(0)
+	//	if err != nil {
+	//		log.Fatalln("Error: setting stdin to raw:", err)
+	//	}
+	//	defer func() {
+	//		if err := terminal.Restore(0, state); err != nil {
+	//			log.Println("Error: failed to restore terminal:", err)
+	//		}
+	//	}()
 
 	in := bufio.NewReader(os.Stdin)
 	//	for {
