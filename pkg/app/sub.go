ff4253b05304bdf3a08c04f2c2e607810cd4a490
Trolbridge <trolbridge.x@gmail.com>
2021-05-07 20:18:21 -0400

    Auto-commit: saved pkg/app/sub.go

--- pkg/app/sub.go
+++ pkg/app/sub.go
@@ -9,32 +9,40 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-func PrintLine() {
-	println("print line test message")
-}
-
 // fd 0 is stdin
 func Listen() rune {
-	state, err := terminal.MakeRaw(0)
-	fmt.Println("setting stdin to raw")
-	if err != nil {
-		log.Fatalln("setting stdin to raw:", err)
-	}
+	    // fd 0 is stdin
+		    state, err := terminal.MakeRaw(0)
+			    if err != nil {
+					        log.Fatalln("setting stdin to raw:", err)
+							    }
+								    defer func() {
+										        if err := terminal.Restore(0, state); err != nil {
+													            log.Println("warning, failed to restore terminal:", err)
+																        }
+																		    }()
 
-	in := bufio.NewReader(os.Stdin)
-	r, _, err := in.ReadRune()
-	if err != nil {
-		log.Println("stdin:", err)
-		if err := terminal.Restore(0, state); err != nil {
-			log.Println("warning, failed to restore terminal:", err)
-		}
-		os.Exit(1)
-	}
+																			    in := bufio.NewReader(os.Stdin)
+																				    for {
+																						        r, _, err := in.ReadRune()
+																								        if err != nil {
+																											            log.Println("stdin:", err)
+																														            break
+																																	        }
+																																			        fmt.Printf("read rune %q\r\n", r)
+																																					        if r == 'q' {
+																																								            break
+																																							}
+																										}
+																					}
+												}
+									}
+				}
 	return r
 
 }
+
 func Quit() {
-	state, err := terminal.MakeRaw(0)
 	fmt.Println("setting stdin to raw")
 	if err != nil {
 		log.Fatalln("setting stdin to raw:", err)
