26a078ea39ac6834f61e9a1a75e6198b818c1e8b
Trolbridge <trolbridge.x@gmail.com>
2021-05-07 20:20:00 -0400

    Auto-commit: saved pkg/app/sub.go

--- pkg/app/sub.go
+++ pkg/app/sub.go
@@ -11,30 +11,29 @@ import (
 
 // fd 0 is stdin
 func Listen() rune {
-	    // fd 0 is stdin
-		    state, err := terminal.MakeRaw(0)
-			    if err != nil {
-					        log.Fatalln("setting stdin to raw:", err)
-							    }
-								    defer func() {
-										        if err := terminal.Restore(0, state); err != nil {
-													            log.Println("warning, failed to restore terminal:", err)
-																        }
-																		    }()
+	// fd 0 is stdin
+	state, err := terminal.MakeRaw(0)
+	if err != nil {
+		log.Fatalln("setting stdin to raw:", err)
+	}
+	defer func() {
+		if err := terminal.Restore(0, state); err != nil {
+			log.Println("warning, failed to restore terminal:", err)
+		}
+	}()
 
-																			    in := bufio.NewReader(os.Stdin)
-																				    for {
-																						        r, _, err := in.ReadRune()
-																								        if err != nil {
-																											            log.Println("stdin:", err)
-																														            break
-																																	        }
-																																			        fmt.Printf("read rune %q\r\n", r)
-																																					        if r == 'q' {
-																																								            break
-																																							}
-																										}
-																					}
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, _, err := in.ReadRune()
+		if err != nil {
+			log.Println("stdin:", err)
+			break
+		}
+		fmt.Printf("read rune %q\r\n", r)
+		if r == 'q' {
+			break
+		}
+	}
 	return r
 
 }
