a40c1bd812cfc70ff231d7143ae5caabb7a4dbf8
Trolbridge <trolbridge.x@gmail.com>
2021-05-09 09:55:09 -0400

    Auto-commit: saved cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -11,9 +11,9 @@ func main() {
 	//	_ = r
 	fmt.Println("Welcome to My-Go")
 	fmt.Println("1. Hello World")
-	fmt.Println("1. Variables")
-	fmt.Println("2. Println")
-	fmt.Println("1. Variables")
+	fmt.Println("2. Variables")
+	fmt.Println("3. Println")
+	fmt.Println("#. Variables")
 	fmt.Println("1. Variables")
 	fmt.Println("1. Variables")
 	fmt.Println("1. Variables")
